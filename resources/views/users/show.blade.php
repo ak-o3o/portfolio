fb5742912cb370cfc1c8fcdf42cb81890376b45a
unknown <nnu.unsung@gmail.com>
2020-10-15 04:11:39 +0900

    20201015 3rd deploy

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,7 +13,13 @@
                     <div class="card">
                         <div class="d-inline-flex">
                             <div class="p-3 d-flex flex-column">
+                                
+                                @if ($user->image_file !== null)
                                 <img src="{{ asset('/storage/user_images/'. $user->image_file) }}" class="rounded-circle" width="100" height="100">
+                                @else
+                                <img src="{{ asset('/storage/default_user_img/default_user.png') }}" class="rounded-circle" width="100" height="100">
+                                @endif
+                                
                                 <div class="mt-3 d-flex flex-column">
                                     <h4 class="mb-0 font-weight-bold">{{ $user->name }}</h4>
                                 </div>
