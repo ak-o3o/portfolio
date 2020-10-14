5d46309c548581ebee8be1158140c79c306b37df
unknown <nnu.unsung@gmail.com>
2020-10-15 06:38:50 +0900

    20201015 11 deploy

--- database/migrations/2020_08_03_022719_create_vocabularies_table.php
+++ database/migrations/2020_08_03_022719_create_vocabularies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVocabularysTable extends Migration
+class CreateVocabulariesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateVocabularysTable extends Migration
      */
     public function up()
     {
-        Schema::create('vocabularys', function (Blueprint $table) {
+        Schema::create('vocabularies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('user_id');
             $table->string('vocabulary_ja', 100);
@@ -29,6 +29,6 @@ class CreateVocabularysTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('vocabularys');
+        Schema::dropIfExists('vocabularies');
     }
 }
